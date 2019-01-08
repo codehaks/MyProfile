50b0e88b2de63156e94d6b80823db097c4a93a72
Hakim Ghods <ghods.hakim@hotmail.com>
2019-01-08 13:51:36 +0330

    Fix

--- MyProfile/Controllers/HomeController.cs
+++ MyProfile/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using MyProfile.Models;
 
 namespace MyProfile.Controllers
 {
@@ -10,9 +11,13 @@ namespace MyProfile.Controllers
     {
         public IActionResult Index()
         {
-            ViewData["message"] = "Hello from Controller!";
+            //ViewData["message"] = "Hello from Controller!";
             ViewData["now"] = DateTime.Now.TimeOfDay;
-            return View();
+
+            var model = new Message();
+            model.Body = "Hello from Model!";
+
+            return View(model);
         }
     }
 }
---
+++ MyProfile/Models/Message.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProfile.Models
+{
+    public class Message
+    {
+        public string Body { get; set; }
+    }
+}
