6ccc2828f1704367e237b743719dfd5076598981
Hakim Ghods <ghods.hakim@hotmail.com>
2019-01-08 12:02:03 +0330

    Sample

---
+++ MyProfile/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyProfile.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["message"] = "Hello from Controller!";
+            return View();
+        }
+    }
+}
