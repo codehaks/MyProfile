81f98fe0c419e34d07d3c0dbde3275b3922ca434
Hakim Ghods <ghods.hakim@hotmail.com>
2019-01-10 15:59:05 +0330

    User Model

--- MyProfile/Data/ProfileDbContext.cs
+++ MyProfile/Data/ProfileDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using MyProfile.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,6 +14,9 @@ namespace MyProfile.Data
             : base(options)
         {
         }
+
+        public DbSet<User> Users { get; set; }
+
 
     }
 }
---
+++ MyProfile/Migrations/20190110122616_UserModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyProfile.Migrations
+{
+    public partial class UserModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
