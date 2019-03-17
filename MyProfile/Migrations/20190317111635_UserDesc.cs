f058edb912a44564e155b612ee66bb5cff001bcc
Hakim Ghods <ghods.hakim@hotmail.com>
2019-03-17 14:54:59 +0330

    User Description

---
+++ MyProfile/Migrations/20190317111635_UserDesc.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MyProfile.Migrations
+{
+    public partial class UserDesc : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Users",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Users");
+        }
+    }
+}
--- MyProfile/Models/User.cs
+++ MyProfile/Models/User.cs
@@ -10,5 +10,6 @@ namespace MyProfile.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public int Age { get; set; }
+        public string Description { get; set; }
     }
 }
