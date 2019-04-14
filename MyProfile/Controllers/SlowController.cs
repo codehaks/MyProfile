59635a5cb3a858958a9637fbb45d89f92f3babf9
Hakim Ghods <ghods.hakim@hotmail.com>
2019-04-14 20:11:58 +0430

    Fix

---
+++ MyProfile/Controllers/SlowController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyProfile.Controllers
+{
+    public class SlowController : Controller
+    {
+        [Route("/slow-async")]
+        public async Task<IActionResult> SlowAsync()
+        {
+            await Task.Delay(TimeSpan.FromSeconds(1));
+            return Ok("async");
+        }
+
+        [Route("/slow-sync")]
+        public IActionResult SlowSync()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(1));
+            return Ok("sync");
+        }
+    }
+}
--- MyProfile/Program.cs
+++ MyProfile/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
@@ -14,6 +15,7 @@ namespace MyProfile
     {
         public static void Main(string[] args)
         {
+            ThreadPool.SetMaxThreads(10, 100);
             CreateWebHostBuilder(args).Build().Run();
         }
 
