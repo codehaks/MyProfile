f521bdb98f59d31374375d0054c8648fc9b7f154
Hakim Ghods <ghods.hakim@hotmail.com>
2019-12-21 17:18:08 +0330

    Spec added

---
+++ MyProfile/Common/ISpecification.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyProfile.Common
+{
+    public interface ISpecification<T>
+    {
+        bool IsSatisfiedBy(T Entity);
+    }
+}
---
+++ MyProfile/Common/OrSpecification.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyProfile.Common
+{
+    public class OrSpecification<T> : ISpecification<T>
+    {
+        private ISpecification<T> first;
+        private ISpecification<T> second;
+
+        public OrSpecification(ISpecification<T> first, ISpecification<T> second)
+        {
+            this.first = first;
+            this.second = second;
+        }
+        public bool IsSatisfiedBy(T entity)
+
+        {
+            return first.IsSatisfiedBy(entity) || second.IsSatisfiedBy(entity);
+        }
+    }
+}
